84fc967203c57d395c43b2d42772763d3fcce5bd
muxingxing07 <1582579709@qq.com>
2020-10-02 21:07:46 +0800

    pop,倒序遍历

--- 27removeelements.py
+++ 27removeelements.py
@@ -3,3 +3,11 @@ class Solution:
         while(val in nums):
             nums.pop(nums.index(val))
         return len(nums)
+
+##倒序遍历
+class Solution:
+    def removeElement(self,nums:List[int],val:int)->int:
+        for i in range(len(nums)-1,-1,-1):
+            if(nums[i]==val):
+                nums.pop(i)
+        return len(nums)
