5a482f5cbf7a2f8950e8664e107fb3a13ac00a2c
muxingxing07 <1582579709@qq.com>
2020-10-02 19:10:31 +0800

    modification

--- jewelandstone.py
+++ jewelandstone.py
@@ -1,7 +1,3 @@
 class Solution:
     def numJewelsInStones(self, J: str, S: str) -> int:
         return sum(s in J for s in S)
-
-    def minimumOperations(self, leaves: str) -> int:
-        n = len(leaves)
-        f = [[0, 0, 0] for _ in range(n)]
