434cd3fb9140a79b3fed16f124628bca8b3e05d0
muxingxing07 <1582579709@qq.com>
2020-10-03 18:29:04 +0800

    pop,倒序遍历

---
+++ threesum.py
@@ -0,0 +1,28 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        result = []
+        if (not nums or n < 3):
+            return []
+        nums.sort()
+        for i in range(n):
+            if (nums[i] > 0):
+                return result
+            if (i > 0 and nums[i] == nums[i - 1]):
+                continue
+            L = i + 1
+            R = n - 1
+            while (L < R):
+                if (nums[i] + nums[L] + nums[R] == 0):
+                    result.append([nums[i], nums[L], nums[R]])
+                    while (L < R and nums[L] == nums[L + 1]):
+                        L = L + 1
+                    while (L < R and nums[R] == nums[R - 1]):
+                        R = R - 1
+                    L = L + 1
+                    R = R - 1
+                elif (nums[i] + nums[L] + nums[R] > 0):
+                    R = R - 1
+                else:
+                    L = L + 1
+        return result
