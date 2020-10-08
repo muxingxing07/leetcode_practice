63f5ddcee1d62c9b5c6b71a80f9aeaa864bc5ff2
muxingxing07 <1582579709@qq.com>
2020-10-08 21:07:11 +0800

    简单题，双指针

---
+++ 136singleNumber.py
@@ -0,0 +1,21 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+
+        for i in nums:
+            if nums.count(i)==1:
+                return i
+
+#位运算：异或运算
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        return reduce(lambda x, y: x ^ y, nums)
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        for i in range(len(nums)):
+            if i == 0:
+                res = nums[i]
+            else:
+                res ^= nums[i]
+
+        return res
