f62e7cfa0b6edfaee8ce35f6bbb96a7df5339bdf
muxingxing07 <1582579709@qq.com>
2020-10-08 21:16:18 +0800

    异或运算，保留单个数

--- 136singleNumber.py
+++ 136singleNumber.py
@@ -5,7 +5,7 @@ class Solution:
             if nums.count(i)==1:
                 return i
 
-##位运算：异或运算
+##位运算：异或运算，相同的数异或为0，0异或任何数为任何数
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         return reduce(lambda x, y: x ^ y, nums)
