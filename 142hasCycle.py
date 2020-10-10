838e3d05f63db203c3db0aa44d827596e293aa7c
muxingxing07 <1582579709@qq.com>
2020-10-10 22:50:34 +0800

    链表问题，快慢指针，两次相遇

--- 142hasCycle.py
+++ 142hasCycle.py
@@ -1,4 +1,6 @@
-##快慢指针，两次相遇
+#解题思路：这类链表题目一般都是使用双指针法解决的，
+#例如寻找距离尾部第K个节点、寻找环入口、寻找公共尾部入口等。
+#快慢指针，两次相遇
 class Solution:
     def detectCycle(self, head: ListNode) -> ListNode:
         fast, slow = head, head
