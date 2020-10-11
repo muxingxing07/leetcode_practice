02c9e0aecce5ccec4f9b31d1ad7db813544dd016
muxingxing07 <1582579709@qq.com>
2020-10-11 19:53:43 +0800

    动态规划，难

---
+++ 416canPartition.py
@@ -0,0 +1,18 @@
+# 经典的NP完全问题(不是很懂)
+# 动态规划
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        n = len(nums)
+        if n < 2:
+            return False
+
+        total = sum(nums)
+        if total % 2 != 0:
+            return False
+
+        target = total // 2
+        dp = [True] + [False] * target
+        for i, num in enumerate(nums):
+            for j in range(target, num - 1, -1):
+                dp[j] |= dp[j - num]
+        return dp[target]
