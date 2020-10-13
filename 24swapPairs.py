69f020bb92de36ab5a46864c050a5a572602bd06
muxingxing07 <1582579709@qq.com>
2020-10-13 19:36:45 +0800

    链表，循环

---
+++ 24swapPairs.py
@@ -0,0 +1,12 @@
+#循环
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        thead = ListNode(-1)
+        thead.next = head
+        c = thead
+        while c.next and c.next.next:
+            a, b = c.next, c.next.next
+            c.next, a.next = b, b.next
+            b.next = a
+            c = c.next.next
+        return thead.next
