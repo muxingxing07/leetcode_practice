f50c64729052184a435a95c665b16d007b93c59a
muxingxing07 <1582579709@qq.com>
2020-10-18 15:33:43 +0800

    链表问题，计算链表长度或者双指针法

---
+++ 19removeNthFromEnd.py
@@ -0,0 +1,38 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        def getLength(head: ListNode) -> int:
+            length = 0
+            while head:
+                length += 1
+                head = head.next
+            return length
+
+        dummy=ListNode(0,head)
+        length=getLength(head)
+        cur=dummy
+        for i in range(1,length-n+1):
+            cur=cur.next
+        cur.next=cur.next.next
+        return dummy.next
+
+##双指针
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        dummy = ListNode(0, head)
+        first = head
+        second = dummy
+        for i in range(n):
+            first = first.next
+
+        while first:
+            first = first.next
+            second = second.next
+
+        second.next = second.next.next
+        return dummy.next
+
